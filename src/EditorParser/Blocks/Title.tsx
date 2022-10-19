9fae75ab7a17364d9e7c88dd1ba6e88bbe6c1733
Mohamed Gad <59421219+mjad218@users.noreply.github.com>
2022-10-19 12:09:34 +0200

    remove br tags and other html tags

--- src/EditorParser/Blocks/Title.tsx
+++ src/EditorParser/Blocks/Title.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { removeTags } from "../../helpers";
 const TitleBlock = (props: { block: any }) => {
   const block = props.block;
-  const text = block.data?.text;
+  const text = removeTags(block.data?.text);
   return <h2>{text}</h2>;
 };
 
